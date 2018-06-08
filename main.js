ea2eba919020419b6534366e4fbee21bd5474b4b
shivangidas <shivangi.das7@gmail.com>
2018-06-08 23:39:50 +0530

    draw on canvas

--- main.js
+++ main.js
@@ -1,5 +1,55 @@
 'use strict';
 import $ from 'jquery';
-$(document).ready(function (){
-    console.log('hello world');
+$(document).ready(function () {
+    //console.log('hello world');
+    var mousePressed = false;
+    var lastX, lastY;
+    var ctx;
+    InitThis();
+    function InitThis() {
+        ctx = document.getElementById('myCanvas').getContext("2d");
+
+        $('#myCanvas').mousedown(function (e) {
+            mousePressed = true;
+            Draw(e.pageX - $(this).offset().left, e.pageY - $(this).offset().top, false);
+        });
+
+        $('#myCanvas').mousemove(function (e) {
+            if (mousePressed) {
+                Draw(e.pageX - $(this).offset().left, e.pageY - $(this).offset().top, true);
+            }
+        });
+
+        $('#myCanvas').mouseup(function (e) {
+            mousePressed = false;
+        });
+        $('#myCanvas').mouseleave(function (e) {
+            mousePressed = false;
+        });
+    }
+
+    function Draw(x, y, isDown) {
+        if (isDown) {
+            ctx.beginPath();
+            ctx.strokeStyle = 'black'
+            ctx.lineWidth = 5;
+            ctx.lineJoin = "round";
+            ctx.moveTo(lastX, lastY);
+            ctx.lineTo(x, y);
+            ctx.closePath();
+            ctx.stroke();
+        }
+        lastX = x;
+        lastY = y;
+    }
+
+    function clearArea() {
+        // Use the identity matrix while clearing the canvas
+        ctx.setTransform(1, 0, 0, 1, 0, 0);
+        ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
+    }
+    $('.clearButton').click(function(event){
+        event.preventDefault();
+        clearArea();
+    })
 });
